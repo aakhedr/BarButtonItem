64e8ade4a31eee1e835d6e495154e7fcdc46ed74
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2018-05-02 17:48:14 +0200

    Initial Commit

---
+++ BarButtonItem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarButtonItem
+//
+//  Created by Ahmed Khedr on 5/2/18.
+//  Copyright © 2018 Ahmed Khedr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
