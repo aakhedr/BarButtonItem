1c427afac151fedefc7a56ba209a85acb9410f5b
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2018-05-02 19:23:07 +0200

    Complete sample - toggling between two fonts shipped with all iOS versions

--- BarButtonItem/AppDelegate.swift
+++ BarButtonItem/AppDelegate.swift
@@ -8,39 +8,57 @@
 
 import UIKit
 
+let userDefaultsKeyForFontName = "font"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        appearance()
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    private func appearance() {
+        let baseFont = checkForSavedFont()
+        
+        /*
+         Appearance proxies on
+         1. navigation bar
+         2. bar button items
+         work as expected on app launch.
+         */
+        
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSAttributedStringKey.font : baseFont as Any
+        ]
+        
+        for controlState in [UIControlState.disabled, .highlighted, .normal] {
+            UIBarButtonItem.appearance().setTitleTextAttributes([
+                NSAttributedStringKey.font : baseFont as Any
+                ]
+                , for: controlState)
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    private func checkForSavedFont() -> UIFont {
+        var fontName: String
+        
+        if let savedFontName = UserDefaults.standard.string(forKey: userDefaultsKeyForFontName) {
+            fontName = savedFontName
+        } else {
+            fontName = "Palatino"
+            
+            UserDefaults.standard.set(fontName, forKey: userDefaultsKeyForFontName)
+        }
+        
+        guard let baseFont = UIFont(name: fontName, size: 15) else {
+            fatalError("Change the two fonts in view controller to a font names that exist!")
+        }
+        
+        return baseFont
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
--- BarButtonItem/ViewController.swift
+++ BarButtonItem/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selectButton: UIBarButtonItem!
+    @IBOutlet weak var updateButton: UIBarButtonItem!
+    @IBOutlet weak var changeFontButton: UIButton!
+    
+    private let palatinoFontName = "Palatino"
+    private let anotherFontName = "CourierNewPSMT"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupChangeFontButton()
+        title = "This title changes font"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeFont(_ sender: UIButton) {
+        guard let currentFontName = UserDefaults.standard.string(forKey: userDefaultsKeyForFontName) else { return }
+        
+        if currentFontName == palatinoFontName {
+            setFont(anotherFontName)
+        } else {
+            setFont(palatinoFontName)
+        }
     }
-
-
+    
+    private func setFont(_ fontName: String) {
+        guard let font = UIFont(name: fontName, size: 15) else {
+            fatalError("Change the '\(fontName)' to a font name that exists!")
+        }
+        
+        // Navigation Bar title changes its font as expected
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedStringKey.font : font as Any
+        ]
+        
+        /*
+         ***** The problem is here ******
+         
+         Bar button items do not get affected by setting the title text attributes
+         It seems that the project HAS TO either:
+         1. be re-run or
+         2. the bar button item title HAS TO change
+         for the new text attributes to take effect?
+        */
+        for controlState in [UIControlState.disabled, .highlighted, .normal] {
+            updateButton.setTitleTextAttributes([
+                NSAttributedStringKey.font : font as Any
+                ], for: controlState)
+            
+            selectButton.setTitleTextAttributes([
+                NSAttributedStringKey.font : font as Any
+                ], for: controlState)
+        }
+        
+        UserDefaults.standard.set(fontName, forKey: userDefaultsKeyForFontName)
+    }
+    
+    private func setupChangeFontButton() {
+        changeFontButton.titleLabel?.textColor = .black
+        changeFontButton.layer.borderWidth = 1
+        changeFontButton.layer.cornerRadius = 5
+        changeFontButton.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
 }
 
